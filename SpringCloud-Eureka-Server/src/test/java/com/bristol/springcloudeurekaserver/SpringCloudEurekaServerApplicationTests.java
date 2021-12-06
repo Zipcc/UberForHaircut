d5fe332a50aa46285c063a3fc45d8e5d568ba7c9
Zipcc <ru20882@bristol.ac.uk>
2021-12-06 23:18:11 +0800

    Father Module Eureka Module Provider-payment Module

---
+++ SpringCloud-Eureka-Server/src/test/java/com/bristol/springcloudeurekaserver/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bristol.springcloudeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
